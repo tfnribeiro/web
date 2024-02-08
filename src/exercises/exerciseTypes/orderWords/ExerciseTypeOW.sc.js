d2cc905a8aac3a9fac017d0b9c72e1ba512f4a71
Tiago Ribeiro <tfnribeiro@gmail.com>
2024-02-08 11:57:28 +0100

    Use Translatable text in orderWords

--- src/exercises/exerciseTypes/orderWords/ExerciseTypeOW.sc.js
+++ src/exercises/exerciseTypes/orderWords/ExerciseTypeOW.sc.js
@@ -21,10 +21,16 @@ const ExerciseOW = styled.div`
   }
 
   .translatedText {
-    font-size: medium;
+    font-size: medium !important;
     margin-left:8px;
     margin-right:8px;
+  }
 
+  .solutionText {
+    font-size: medium !important;
+    color: gray;
+    margin-right: 30px;
+    margin-left: 30px;
   }
 
   .headlineWithMoreSpace {
@@ -70,7 +76,6 @@ const ExerciseOW = styled.div`
     color: gray;
     margin-top: 1.5em;
     margin-bottom: 1em;
-
 
     /* font-weight: 600; */
     h2 {
